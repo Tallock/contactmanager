1a50f593af2848875768cf8e0842308faee8c2d1
Glenn Obeng <>
2019-02-07 17:18:25 +0100

    Updates

---
+++ precache-manifest.5e87d726db39a9fa13b0f4eed421b758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "63fbadfcb16e55be0800",
+    "url": "/contactmanager/static/js/main.63fbadfc.chunk.js"
+  },
+  {
+    "revision": "0a6aadacfcd42b4b8b63",
+    "url": "/contactmanager/static/js/1.0a6aadac.chunk.js"
+  },
+  {
+    "revision": "63fbadfcb16e55be0800",
+    "url": "/contactmanager/static/css/main.4d06353b.chunk.css"
+  },
+  {
+    "revision": "0a6aadacfcd42b4b8b63",
+    "url": "/contactmanager/static/css/1.b0685c6d.chunk.css"
+  },
+  {
+    "revision": "84e6172a3a10b3fab516e48e6505fe6d",
+    "url": "/contactmanager/index.html"
+  }
+];
